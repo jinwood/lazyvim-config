335a3f8a607bed0bc4fda4b8bd90f6bc41f37cd6
jinwood <jinw@protonmail.com>
2023-07-13 10:46:10 +0100

    Init

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+-- themes I like
+
+return {
+  { "aktersnurra/no-clown-fiesta.nvim" },
+  { "ntk148v/komau.vim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "no-clown-fiesta",
+    },
+  },
+}
