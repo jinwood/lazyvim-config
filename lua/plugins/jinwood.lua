cbd3a9e7d1d164b428888bb7717159978c8ecafd
jinwood <jinw@protonmail.com>
2023-09-29 14:44:56 +0100

    Add codeium

--- lua/plugins/jinwood.lua
+++ lua/plugins/jinwood.lua
@@ -26,4 +26,12 @@ return {
       },
     },
   },
+  {
+    "Exafunction/codeium.vim",
+    config = function()
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+    end,
+  },
 }
