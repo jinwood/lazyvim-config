14bd8ce68b8bf78b4a1960890af8a9919c42b68b
Julian Inwood <julian.inwood@builder.ai>
2023-11-24 16:41:42 +0000

    Dont hide current level. Enable codeium

--- lua/plugins/jinwood.lua
+++ lua/plugins/jinwood.lua
@@ -15,4 +15,12 @@ return {
       },
     },
   },
+  {
+    "Exafunction/codeium.vim",
+    config = function()
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+    end,
+  },
 }
