c2f730fa94a19d584b21176f27d2b52f8fe07738
Julian Inwood <julian.inwood@builder.ai>
2023-11-24 07:55:46 +0000

    Reset

---
+++ lua/plugins/jinwood.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
