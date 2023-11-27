2e17a462e379ebb6d11daec4708b52812cd1a842
Julian Inwood <julian.inwood@builder.ai>
2023-11-27 11:48:14 +0000

    Fix warning

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,5 +1,4 @@
 return {
-
   -- Correctly setup lspconfig for C# 🚀
   {
     "neovim/nvim-lspconfig",
@@ -8,17 +7,14 @@ return {
         -- Ensure mason installs the server
         omnisharp = {},
       },
-      -- configure omnisharp to fix the semantic tokens bug (really annoying)
       setup = {
         omnisharp = function(_, _)
-          require("lazyvim.util").on_attach(function(client, _)
+          require("lazyvim.util").lsp.on_attach(function(client, _)
             if client.name == "omnisharp" then
-              ---@type string[]
               local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
               for i, v in ipairs(tokenModifiers) do
                 tokenModifiers[i] = v:gsub(" ", "_")
               end
-              ---@type string[]
               local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
               for i, v in ipairs(tokenTypes) do
                 tokenTypes[i] = v:gsub(" ", "_")
