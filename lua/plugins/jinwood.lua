047621d4f6f29b25f9c75d88d37ea4d285f35c41
Julian Inwood <jinw@protonmail.com>
2024-01-30 13:41:11 +0000

    telescope

--- lua/plugins/jinwood.lua
+++ lua/plugins/jinwood.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
