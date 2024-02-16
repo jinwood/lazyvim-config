4999e4d1923a7796b0c68f7e0d8dd3650d52a760
Julian Inwood <jinw@protonmail.com>
2024-02-16 11:08:21 +0000

    Themes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
+    background = "dark",
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
@@ -36,6 +37,37 @@ require("lazy").setup({
         "tutor",
         "zipPlugin",
       },
+    },
+  },
+
+  opts = {
+    servers = {
+      tailwindcss = {
+        -- exclude a filetype from the default_config
+        filetypes_exclude = { "markdown" },
+        -- add additional filetypes to the default_config
+        filetypes_include = {},
+        -- to fully override the default_config, change the below
+        -- filetypes = {}
+      },
+    },
+    setup = {
+      tailwindcss = function(_, opts)
+        local tw = require("lspconfig.server_configurations.tailwindcss")
+        opts.filetypes = opts.filetypes or {}
+
+        -- Add default filetypes
+        vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+        -- Remove excluded filetypes
+        --- @param ft string
+        opts.filetypes = vim.tbl_filter(function(ft)
+          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+        end, opts.filetypes)
+
+        -- Add additional filetypes
+        vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+      end,
     },
   },
 })
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "jaredgorski/fogbell.vim",
+  },
+  {
+    "logico/typewriter",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "256-grayvim",
+    },
+  },
+  {
+    "flazz/vim-colorschemes",
+  },
+  {
+    "Alligator/accent.vim",
+  },
+}
