b0fbfea515358ca22bad822dc3fe4a13b989b280
Julian Inwood <jinw@protonmail.com>
2024-05-02 10:35:37 +0100

    Fix telescope not using root dir

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.conceallevel = 0 -- don't hide my json strings
+vim.g.root_spec = { "cwd" }
