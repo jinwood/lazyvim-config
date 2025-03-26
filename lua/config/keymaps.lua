2cb8571edbbd493b00158c86d6a885c9d75dfcb0
jinwood <jinw@protonmail.com>
2025-03-26 20:42:04 +0000

    Add guid plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.del("i", "<leader>t")
+vim.keymap.set("n", "<leader>G", ":InsertGUID<CR>")
 local map = vim.keymap.set
--- lua/plugins/jinwood.lua
+++ lua/plugins/jinwood.lua
@@ -53,4 +53,7 @@ return {
       inlay_hints = { enabled = false },
     },
   },
+  {
+    "jinwood/nvim-guid",
+  },
 }
