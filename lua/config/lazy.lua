b1692e6be34fefc85555eca216140a45512d26ee
jinwood <jinw@protonmail.com>
2025-07-10 13:20:28 +0100

    Nothin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
     background = "dark",
+    colorscheme = "default",
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
