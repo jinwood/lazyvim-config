978bdfe07a34f6d772374c42adc979eabb3a5301
jinwood <jinw@protonmail.com>
2026-02-26 08:47:34 +0000

    feat: switch off inlay hints

--- lua/plugins/jinwood.lua
+++ lua/plugins/jinwood.lua
@@ -18,6 +18,26 @@ return {
         vtsls = {
           filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
           settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = { enabled = "none" },
+                parameterTypes = { enabled = false },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                enumMemberValues = { enabled = false },
+              },
+            },
+            javascript = {
+              inlayHints = {
+                parameterNames = { enabled = "none" },
+                parameterTypes = { enabled = false },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                enumMemberValues = { enabled = false },
+              },
+            },
             vtsls = {
               filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
               settings = {
